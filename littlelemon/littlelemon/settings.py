e18d2294c0ffb729b6daef442ed101914ab6e5b3
vanivasanth <gogila.vasanth@gmail.com>
2025-08-30 15:02:46 +0530

    db changes

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -94,7 +94,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'LittleLemon',
         'USER': 'root',
-        'PASSWORD': 'vaniV@santh68',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'OPTIONS': {
